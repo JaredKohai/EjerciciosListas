e4b0e22a3be10f2f2596eb0e6cee1057156049a2
Jared <jaredbastarracheas@gmail.com>
2023-10-17 08:32:14 -0600

    awa

--- EjerciciosListas/Ejercicio4.cs
+++ EjerciciosListas/Ejercicio4.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace EjerciciosListas
+namespace EjerciciosListas//
 {
     public partial class Ejercicio4 : Form
     {
